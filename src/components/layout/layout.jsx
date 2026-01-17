fe208d624fcfb1dc26ab656cf16aee03d2b579df
cyber-sec0 <bcg33@byu.edu>
2026-01-17 22:07:55 +0000

    .

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,9 @@
+/**
+ * @namespace Layout
+ * @description Components for the application layout structure
+ */
+
+export { default as NavigationBar } from './NavigationBar';
+export { default as Footer } from './Footer';
+export { default as PageContainer } from './PageContainer';
+export { default as ThemeToggle } from './ThemeToggle';
