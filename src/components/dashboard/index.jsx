a40aaaa9c54e3fb9ec7d0c8bef94f99ba4e84a16
cyber-sec0 <bcg33@byu.edu>
2026-01-17 21:02:01 +0000

    .

---
+++ src/components/dashboard/index.jsx
@@ -0,0 +1,10 @@
+/**
+ * @namespace Dashboard
+ * @description Components for displaying and interacting with recipes in the dashboard view
+ */
+
+export { default as RecipeCard } from './RecipeCard';
+export { default as RecipeGrid } from './RecipeGrid';
+export { default as SearchBar } from './SearchBar';
+export { default as PaginationControls } from './PaginationControls';
+export { default as DashboardContainer } from './DashboardContainer';
