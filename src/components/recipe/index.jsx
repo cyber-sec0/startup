4766989635f38253d31a8329ebdfb7c5fcc7adbc
cyber-sec0 <bcg33@byu.edu>
2026-01-17 22:28:24 +0000

    .

---
+++ src/components/recipe/index.jsx
@@ -0,0 +1,8 @@
+/**
+ * @namespace Recipe
+ * @description Components for viewing, creating, and editing individual recipes
+ */
+
+export { default as RecipeActionBar } from './RecipeActionBar';
+export { default as RecipeDetails } from './RecipeDetails';
+export { default as RecipeForm } from './RecipeForm';
