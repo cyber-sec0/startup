d84ee28431b2794bebd4212ca5515f1fb27b35ca
cyber-sec0 <bcg33@byu.edu>
2026-01-27 00:03:37 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,12 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
-    // Add this section:
     allowedHosts: [
       'recipemaster.click'
     ],
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
       },
