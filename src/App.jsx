22badaa94e7a1fcf7d31b8753828527cc01de28a
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-02-07 20:10:08 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import theme from './theme/theme';
 import './App.css';
 import ProtectedRoute from './components/ProtectedRoutes';
 import { AuthProvider } from './contexts/AuthContext';
-// import { AuthProvider } from './contexts/MockAuthContext';
-//Pages Imports
 import PageContainer from './components/layout/PageContainer';
 import DashboardPage from './pages/DashboardPage';
 import RecipePage from './pages/RecipePage';
@@ -17,35 +15,33 @@ import SignUpPage from './pages/SignUpPage';
 import AddRecipePage from './pages/AddRecipePage';
 import LandingPage from './pages/LandingPage';
 import ProfilePage from './pages/ProfilePage';
+import WebSocketTestPage from './pages/WebSocketTestPage';
 import OnboardingTour from './components/common/OnBoardingTour';
-
 import { ThemeProvider } from './contexts/ThemeProvider';
 
 function App() {
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
-        <CssBaseline /> {/* CssBaseline normalizes styles and applies theme background */}
+        <CssBaseline />
           <Router>
             <div className="App">
               <PageContainer>
                 <OnboardingTour />
                 <Routes>
-                  {/* Public Routes */}
                   <Route path="/signin" element={<SignInPage />} />
                   <Route path="/signup" element={<SignUpPage />} />
                   <Route path="/" element={<LandingPage/>} />
+                  <Route path="/ws-test" element={<WebSocketTestPage />} />
 
-                  {/* Protected routes - only accessible when logged in */}
                   <Route element={<ProtectedRoute />}>
                     <Route path="/recipe/:id" element={<RecipePage />} />
                     <Route path="/api" element={<ApiTestPage />} />
                     <Route path="/edit-recipe/:id" element={<EditRecipePage />} />
                     <Route path="/add" element={<AddRecipePage />} />
-                    <Route path="/add" element={<AddRecipePage />} />
                     <Route path="/profile" element={<ProfilePage />} />
                     <Route path="/dash" element={<DashboardPage />} />
-                    </Route>
+                  </Route>
                 </Routes>
               </PageContainer>
             </div>
