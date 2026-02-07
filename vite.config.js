bfc8d9c18c89b6ffcc3022df5c20489d8cbc3bff
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2026-02-07 22:02:43 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+      '/ws': {
+        target: 'ws://localhost:4000',
+        ws: true,
+        changeOrigin: true,
+      },
     },
   },
 });
